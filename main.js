aef77b58682c473af79cee5d957afcb96272abbd
alawld <1679748907@qq.com>
2018-01-05 15:17:24 +0800

    完成代码优化

--- main.js
+++ main.js
@@ -1,17 +1,34 @@
-$(' .images > img:nth-child(1)').addClass('current')
-$(' .images > img:nth-child(2)').addClass('enter')
-$(' .images > img:nth-child(3)').addClass('enter')
-$(' .images > img:nth-child(4)').addClass('enter')
-let n = 1;
-
+let n
+初始化()
 setInterval(()=>{
-    $(` .images > img:nth-child(${x(n)})`).removeClass('current')
-        .addClass('leave').one('transitionend', (e)=>{
-            $(e.currentTarget).removeClass('leave').addClass('enter')
+    makeLeave(getImage(n)).one('transitionend', (e)=>{
+            makeEnter($(e.currentTarget))
         })
-    $(` .images > img:nth-child(${x(n + 1)})`).removeClass('enter').addClass('current')  
+    makeCurrent(getImage(n+1))
     n += 1    
 }, 2000)
+
+
+
+/* 下发代码可以不看 */
+function 初始化(){
+    n=1
+    $(`.images > img:nth-child(${n})`).addClass('current')
+        .siblings().addClass('enter')
+}
+
+function getImage(n) {
+    return $(` .images > img:nth-child(${x(n)})`)  // ``插值法${}
+}
+function makeLeave($node) {
+    return $node.removeClass('current enter').addClass('leave')
+}
+function makeCurrent($node) {
+    return $node.removeClass('leave enter').addClass('current')
+}
+function makeEnter($node) {
+    return $node.removeClass('current leave').addClass('enter')
+}
 
 function x(n){
     if(n>4){
